fb5b3e0093a43cd309ccc9a8e1c8179267d254ac
csslick <digitone@naver.com>
2025-04-30 18:07:32 +0900

    1.setup

---
+++ src/main.js
@@ -0,0 +1,22 @@
+import './style.css'
+import Dropdown from './Dropdown'
+
+document.querySelector('#app').innerHTML = `
+  <div class="flex flex-col items-center justify-center min-h-screen py-2">
+    <h1 class="text-3xl font-bold mb-2">Accordion Menu</h1>
+    <nav class='flex'></nav>
+  </div>
+  
+`
+
+Dropdown(
+  '#app nav', 
+  'Dropdown menu', 
+  ['C','Java','Python','JavaScript']
+)
+
+Dropdown(
+  '#app nav', 
+  'Dropdown menu', 
+  ['menu1', 'menu2', 'menu3']
+)
